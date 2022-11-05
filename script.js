7e336a3dad300713f5436fb33aa74f30147e9824
Toshi kuro <glay0208@gmail.com>
2022-11-05 22:28:59 +0900

    complete

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 $(function(){
-  /*=================================================
-  スマホメニュー
-  ===================================================*/
+
   // ハンバーガーメニューのクリックイベント
-  // 解説は、「中級編：ストアサイト（インテリア）」参照
+
   $('.toggle_btn').on('click', function() {
     if ($('#header').hasClass('open')) {
       $('#header').removeClass('open');
@@ -22,63 +20,37 @@ $(function(){
     $('#header').removeClass('open');
   });
 
-  // /*=================================================
-  // スムーススクロール
-  // ===================================================*/
-  // // ページ内リンクのイベント
-  // $('a[href^="#"]').click(function(){
-  //   // リンクを取得
-  //   let href= $(this).attr("href");
-  //   // ジャンプ先のid名をセット
-  //   let target = $(href == "#" || href == "" ? 'html' : href);
-  //   // トップからジャンプ先の要素までの距離を取得
-  //   let position = target.offset().top;
-  //   // animateでスムーススクロールを行う
-  //   // 600はスクロール速度で単位はミリ秒
-  //   $("html, body").animate({scrollTop:position}, 600, "swing");
-  //   return false;
-  // });
+  // ページ内リンクのイベント
 
-  // /*=================================================
-  // PICK UP スライダー
-  // ===================================================*/
-  // // カルーセル用 jQueryプラグイン「slick」の設定
-  // // マニュアル：https://kenwheeler.github.io/slick/
-  // $('.slick-area').slick({
-  //   arrows: false,
-  //   centerMode: true,
-  //   centerPadding: '100px',
-  //   slidesToShow: 3,
-  //   responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         centerPadding: '50px',
-  //         slidesToShow: 1
-  //       }
-  //     }
-  //   ]
-  // });
+  $('a[href^="#"]').click(function(){
+    // リンクを取得
+    let href= $(this).attr("href");
+    // ジャンプ先のid名をセット
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    // トップからジャンプ先の要素までの距離を取得
+    let position = target.offset().top;
+    // animateでスムーススクロールを行う
+    // 600はスクロール速度で単位はミリ秒
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+    return false;
+  });
 
-  // /*=================================================
-  // スクロール時の画像フェード表示
-  // ===================================================*/
-  // // スクロール時のイベント
-  // $(window).scroll(function() {
-  //   // fadeinクラスに対して順に処理を行う
-  //   $('.fadein').each(function() {
-  //     // スクロールした距離
-  //     let scroll = $(window).scrollTop();
-  //     // fadeinクラスの要素までの距離
-  //     let target = $(this).offset().top;
-  //     // 画面の高さ
-  //     let windowHeight = $(window).height();
-  //     // fadeinクラスの要素が画面下にきてから200px通過した
-  //     // したタイミングで要素を表示
-  //     if (scroll > target - windowHeight + 200) {
-  //       $(this).css('opacity','1');
-  //       $(this).css('transform','translateY(0)');
-  //     }
-  //   });
-  // });
+  // スクロール時のイベント
+
+  $(window).scroll(function() {
+    // fade-inクラスに対して順に処理を行う
+    $('.fade-in').each(function() {
+      // スクロール距離
+      let scroll = $(window).scrollTop();
+      // fade-inクラスの要素までの距離
+      let target = $(this).offset().top;
+      // 画面の高さ
+      let windowHeight = $(window).height();
+      // fade-inクラスの要素が画面下にきてから200px通過タイミングで表示
+      if (scroll > target - windowHeight + 200) {
+        $(this).css('opacity','1');
+        $(this).css('transform','translateY(0)');
+      }
+    });
+  });
 });
